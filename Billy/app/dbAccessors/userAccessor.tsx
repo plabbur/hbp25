fb473e1c1a151df4ce1874f028b9e238be0ec41b
adrodvaz04 <isyla.adrian@gmail.com>
2025-02-08 10:29:21 -0500

    Morning commit 1

---
+++ Billy/app/dbAccessors/userAccessor.tsx
@@ -0,0 +1,11 @@
+import { supabase } from "../../lib/AsyncStorage";
+
+/**
+ *  Database accessor for users.
+ *  
+ */
+export default class UserAccessor {
+    // getUserByID
+    // getUserIDByEmail
+    // what else...
+}
